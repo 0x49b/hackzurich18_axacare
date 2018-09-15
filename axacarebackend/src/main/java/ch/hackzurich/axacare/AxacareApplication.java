53f02a58b607e8dbfdddc99438280a24b3376665
rogerkreienbuehl <r.kreienbuehl>
2018-09-15 09:17:12 +0200

    enabled neo4j repositories

--- axacarebackend/src/main/java/ch/hackzurich/axacare/AxacareApplication.java
+++ axacarebackend/src/main/java/ch/hackzurich/axacare/AxacareApplication.java
@@ -2,9 +2,11 @@ package ch.hackzurich.axacare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableNeo4jRepositories("ch.hackzurich.axacare.repositories")
 public class AxacareApplication {
 
 	public static void main(String[] args) {
