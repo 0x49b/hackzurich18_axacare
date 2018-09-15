0674e9d295dff155e73e625be519a13dcf792987
lichtwellenreiter <florianjeremias@gmail.com>
2018-09-15 09:07:52 +0200

    init code push

---
+++ axacarebackend/src/main/java/ch/hackzurich/axacare/AxacareApplication.java
@@ -0,0 +1,13 @@
+package ch.hackzurich.axacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class AxacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxacareApplication.class, args);
+	}
+}
