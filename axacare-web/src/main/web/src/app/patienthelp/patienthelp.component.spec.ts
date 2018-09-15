54904e6f3a3efebf67a3ae6d2c5fc0a7a310359e
lichtwellenreiter <florianjeremias@gmail.com>
2018-09-15 15:47:33 +0200

    added patientrecipe

---
+++ axacare-web/src/main/web/src/app/patienthelp/patienthelp.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PatienthelpComponent } from './patienthelp.component';
+
+describe('PatienthelpComponent', () => {
+  let component: PatienthelpComponent;
+  let fixture: ComponentFixture<PatienthelpComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PatienthelpComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PatienthelpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ axacare-web/src/main/web/src/app/patienthelp/patienthelp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patienthelp',
+  templateUrl: './patienthelp.component.html',
+  styleUrls: ['./patienthelp.component.css']
+})
+export class PatienthelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
